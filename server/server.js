332fc5fe2354919882c1992474119c22dd7d2255
Marcin Polski <marcin.polski98@gmail.com>
2018-01-02 21:22:33 +0100

    Greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -14,8 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    text: 'Welcome to the chat app',
+    from: 'Admin',
+    createdAt: new Date().getTime(),
+  });
+
+  socket.broadcast.emit('newMessage', {
+    text: 'New user joined',
+    from: 'Admin',
+    createdAt: new Date().getTime(),
+  });
+
   socket.on('createMessage', function (message) {
-    io.emit('newMessage', {
+    socket.broadcast.emit('newMessage', {
       text: message.text,
       from: message.from,
       createdAt: new Date().getTime(),
